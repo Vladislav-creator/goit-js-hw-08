f7fb7cab72a3851b7ab130952a547365ae2737f3
Vladislav-creator <valdisbeketov@gmail.com>
2023-08-24 12:33:44 +0300

    99

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const container = document.querySelector('.gallery');
+container.style.listStyle = 'none';
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+  />
+</a>
+</li>`
+  )
+  .join('');
+container.innerHTML = markup;
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 console.log(galleryItems);
